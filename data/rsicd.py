fdea780918d28fc3ca20e96c2a3ddebcfbf61c86
Aliasghar Jawadwala <sharksurfauto@gmail.com>
2026-04-25 17:17:59 +0530

    fix: strip rsicd_images prefix from RSICD filenames

--- data/rsicd.py
+++ data/rsicd.py
@@ -85,7 +85,7 @@ class RSICDDataset(Dataset):
     def __getitem__(self, idx):
         filename, caption = self.pairs[idx]
         clean_name=os.path.basename(filename)
-        img_path = os.path.join(self.image_dir, filename)
+        img_path = os.path.join(self.image_dir, clean_name)
 
         image = Image.open(img_path).convert("RGB")
         image = self.transform(image)
